f2df7f211bfa904edb34bd6ba7f7ec0e96214776
himanshusagar <himanshusagar@protonmail.com>
2022-04-04 22:28:51 -0500

    More 2

--- src/client.cc
+++ src/client.cc
@@ -68,9 +68,9 @@ int main(int argc, char *argv[])
     }
 
     string some = "dummyData";
-    some.resize(MAX_SIZE);
+    some.resize(MAX_SIZE , '.');
     std::cout << "written : " << active_server->SayWrite(20, some.data()) << std::endl;
-    std::cout << "read : " << active_server->SayRead(20, some.data()) << " " << some << std::endl;
+    std::cout << "read : " << active_server->SayRead(20, some.data()) << std::endl;
     std::cout << some << std::endl;
     //std::cout << active_server->PingLeader() << std::endl;
     //std::cout << active_server->PingBackup() << std::endl;
