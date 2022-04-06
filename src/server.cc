16683c4ea9c667b24686cd4e0fc491668e5dd7de
hsagar <hsagar@localhost>
2022-04-06 05:13:38 -0500

    reset backisactive

--- src/server.cc
+++ src/server.cc
@@ -71,6 +71,7 @@ void heartbeat_thread(string address, StoreRPCServiceImpl *service)
             cout<<"wait over"<<endl;
             // waiting till backup/leader comes back alive
             service->failed_heartbeats = 0;
+            service->backupIsActive = true;
             service->connOtherServer = new StoreRPCClient( grpc::CreateCustomChannel(target_str, grpc::InsecureChannelCredentials(), service->ch_args)
                                     , target_str);
             // wait over ?
